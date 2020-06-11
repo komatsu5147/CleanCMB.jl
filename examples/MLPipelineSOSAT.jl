a3843a6fd3676706a53e5379ca7a92f82269f253
Eiichiro Komatsu <eiichirokomatsu@me.com>
2020-06-11 20:48:41 +0200

    ML in harmonic domain

--- examples/MLPipelineSOSAT.jl
+++ examples/MLPipelineSOSAT.jl
@@ -6,21 +6,15 @@ using Random, Statistics
 using Printf
 using Plots, LaTeXStrings
 using Tables, CSV
-# %% Multi-threading computation for maximisation of the likelihood?
-multithreads = true
-if multithreads
-    using Base.Threads
-    println("# of threads available = ", nthreads())
-end
-
 # %% Create figures?
 showresults = true
 
 # %% Simulation parameters
 nrz = 10 # How many realisations?
-#Random.seed!(5147) # Initial random number seed. Useful if you need reproducible sequence
+Random.seed!(5147) # Initial random number seed. Useful if you need reproducible sequence
 rsim = 0 # Tensor-to-scalar ratio used for the simulation
 Alens = 1 # Lensing power spectrum amplitude (Alens = 1 for the fiducial)
+ℓmin, ℓmax = 30, 200 # ℓ range for fitting
 
 # %% Specification of the experiment
 # Reference: Simons Observatory Collaboration, JCAP, 02, 056 (2019), Table 1.
@@ -46,8 +40,11 @@ for ip = 1:12*nside^2
     end
 end
 
-# %% Basic parameters for spherical harmonics transform
+# %% Compute scalar and tensor power spectra using CLASS
 lmax, mmax = 3 * nside - 1, 3 * nside - 1
+include("compute_cl_class.jl")
+
+# %% Basic parameters for spherical harmonics transform
 geom_info = make_healpix_geom_info(nside, 1)
 alm_info = make_triangular_alm_info(lmax, mmax, 1)
 elm, blm = Alm{ComplexF64}(lmax, mmax), Alm{ComplexF64}(lmax, mmax)
@@ -88,92 +85,9 @@ for iν = 1:nν
     push!(f_u, u)
 end
 
-# %% Compute the scalar and tensor CMB power spectrum
-# Call the python wrapper for CLASS, `classy`
-# Reference: https://github.com/lesgourg/class_public
-classy = pyimport("classy")
-# Create an instance of the CLASS wrapper
-cosmo = classy.Class()
-## Scalar power spectrum
-# Create a dictionary of the cosmological parameters
-Tcmb = 2.7255e6 # in μK
-deg_ncdm = true # 3 massive neutrinos with degenerate mass?
-params = Dict(
-    "output" => "tCl pCl lCl",
-    "modes" => "s",
-    "l_max_scalars" => lmax,
-    "lensing" => "yes",
-    "A_s" => exp(3.0448) * 1e-10,
-    "n_s" => 0.96605,
-    "k_pivot" => 0.05,
-    "h" => 0.6732,
-    "omega_b" => 0.022383,
-    "omega_cdm" => 0.12011,
-    "tau_reio" => 0.0543,
-    "N_ncdm" => 1,
-)
-if deg_ncdm
-    push!(params, "m_ncdm" => 0.02, "deg_ncdm" => 3, "N_ur" => 0.00641)
-    mν = params["m_ncdm"] * params["deg_ncdm"]
-else
-    push!(params, "m_ncdm" => 0.06, "N_ur" => 2.0328)
-    mν = params["m_ncdm"]
-end
-# Set the parameters to the cosmological code
-cosmo.set(params)
-# Run the whole code. Depending on your output, it will call the
-# CLASS modules more or less fast. For instance, without any
-# output asked, CLASS will only compute background quantities,
-# thus running almost instantaneously.
-# This is equivalent to the beginning of the `main` routine of CLASS,
-# with all the struct_init() methods called.
-cosmo.compute()
-# Access the lensed scalar cl until l=lmax. It is a dictionnary that contains the fields: ell, tt, te, ee, bb, pp, tp
-cls = cosmo.lensed_cl(lmax)
-# If you want to change completely the cosmology, you should also
-# clean the arguments, otherwise, if you are simply running on a loop
-# of different values for the same parameters, this step is not needed
-cosmo.empty()
-## Tensor power spectrum
-rclass = 0.01
-params = Dict(
-    "output" => "tCl pCl",
-    "modes" => "t",
-    "l_max_tensors" => lmax,
-    "lensing" => "no",
-    "r" => rclass,
-    "n_t" => 0,
-    "A_s" => exp(3.0448) * 1e-10,
-    "k_pivot" => 0.05,
-    "h" => 0.6732,
-    "omega_b" => 0.022383,
-    "omega_cdm" => 0.12011,
-    "tau_reio" => 0.0543,
-    "N_ncdm" => 1,
-)
-if deg_ncdm
-    push!(params, "m_ncdm" => 0.02, "deg_ncdm" => 3, "N_ur" => 0.00641)
-    mν = params["m_ncdm"] * params["deg_ncdm"]
-else
-    push!(params, "m_ncdm" => 0.06, "N_ur" => 2.0328)
-    mν = params["m_ncdm"]
-end
-cosmo.set(params)
-cosmo.compute()
-clt = cosmo.raw_cl(lmax)
-
-# %% Import NaMaster for the power spectrum analysis on a partial sky
-# Reference: Alonso et al.,  MNRAS, 484, 4127 (2019), https://github.com/LSSTDESC/NaMaster
-nmt = pyimport("pymaster")
-# Read in and apodize mask (C^2 apodization, 10 degrees)
+# %% Setup NaMaster for the power spectrum analysis on a partial sky
 maskfile = "data/mask_apodized_r7.fits"
-m = readMapFromFITS(maskfile, 1, Float64)
-mask = nmt.mask_apodization(m.pixels, 10.0, apotype = "C2")
-# Create binning scheme. We will use 10 multipoles per bandpower.
-b = nmt.NmtBin.from_nside_linear(nside, 10, is_Dell = true)
-# Array with effective multipole per bandpower
-ell_eff = b.get_effective_ells()
-nbands = b.get_n_bands()
+include("setup_namaster.jl")
 # The theory power spectrum must be binned into bandpowers in the same manner the data has.
 # Generate an NmtWorkspace object that we use to compute and store the mode coupling matrix.
 # Note that this matrix depends only on the masks of the two fields to correlate, but not on the maps themselves.
@@ -184,21 +98,6 @@ cls_th = [cls["ee"], zero(cls["ee"]), zero(cls["bb"]), cls["bb"]] * Tcmb^2
 cls_th_binned = w.decouple_cell(w.couple_cell(cls_th))
 clt_th = [clt["ee"], zero(clt["ee"]), zero(clt["bb"]), clt["bb"]] * Tcmb^2
 clt_th_binned = w.decouple_cell(w.couple_cell(clt_th))
-# The function defined below will compute the power spectrum between two
-# NmtFields f_a and f_b, using the coupling matrix stored in the
-# NmtWorkspace wsp and subtracting the deprojection bias clb.
-# Note that the most expensive operations in the MASTER algorithm are
-# the computation of the coupling matrix and the deprojection bias. Since
-# these two objects are precomputed, this function should be pretty fast!
-function compute_master(f_a, f_b, wsp)
-    # Compute the power spectrum (a la anafast) of the masked fields
-    # Note that we only use n_iter=0 here to speed up the computation,
-    # but the default value of 3 is recommended in general.
-    cl_coupled = nmt.compute_coupled_cell(f_a, f_b)
-    # Decouple power spectrum into bandpowers inverting the coupling matrix
-    cl_decoupled = wsp.decouple_cell(cl_coupled)
-    return cl_decoupled
-end
 
 # %% Loop over realisations
 ee1, bb1 = zeros(nbands, nrz), zeros(nbands, nrz) # Cleaned power spectra
@@ -226,7 +125,6 @@ for irz = 1:nrz
     n_q, n_u = Map{Float64,RingOrder}(nside), Map{Float64,RingOrder}(nside)
     f1, f2, f3 = [], [], [] # List of NaMaster fields
     nij = zeros(nν, nν) # Noise covariance matrix for the ML analysis
-    d = zeros(nν, 12 * nside^2, 2) # [Q(ν, ip), U(ν, ip)] data matrix
     for iν = 1:nν
         nij[iν, iν] = uKarcmin[iν]^2
         for l = 0:lmax
@@ -293,7 +191,6 @@ for irz = 1:nrz
                 beam = bl,
             ),
         )
-        d[iν, :, 1], d[iν, :, 2] = f_q[iν] + c_q + n_q, f_u[iν] + c_u + n_u
         # f2: noise
         push!(f2, nmt.NmtField(mask, [n_q, n_u], purify_b = true, beam = bl))
         # f3: foreground
@@ -315,77 +212,34 @@ for irz = 1:nrz
         ce2[iν, jν, :], cb2[iν, jν, :] = cl2[1, :], cl2[4, :]
         ce3[iν, jν, :], cb3[iν, jν, :] = cl3[1, :], cl3[4, :]
     end
-    ## Maximise the profile likelihood for the foreground parameters
-    # Frequency response matrix and derivatives
-    A(x) = [ones(nν) synch.(ν, βs = x[1]) dust1.(ν, βd = x[2])]
-    dAdβs(x) = [zeros(nν) synch.(ν, βs = x) .* log.(ν / 23) zeros(nν)]
-    dAdβd(x) = [zeros(nν) zeros(nν) dust1.(ν, βd = x) .* log.(ν / 353)]
-    ip = findall(x -> x > 0, weight)
-    np = length(ip)
-    # Function to maximise by `optimize`
-    function func(x)
-        add(iip) =
-            loglike_beta(nij * weight[ip[iip]]^2, A(x), d[:, ip[iip], 1]) +
-            loglike_beta(nij * weight[ip[iip]]^2, A(x), d[:, ip[iip], 2])
-        if multithreads
-            lnlike = Atomic{Float64}(0)
-            @threads for iip = 1:np
-                atomic_add!(lnlike, add(iip))
-            end
-            return -lnlike[]
-        else
-            lnlike = sum(add.(1:np))
-            return -lnlike
-        end
+    ## Use Parametric Maximum Likelihood method to obtain power spectra of clean maps of the CMB
+    A(x) = [ones(nν) synch.(ν, βs = x[1]) dust1.(ν, βd = x[2])] # Frequency response matrix
+    ibmax = maximum(findall(x -> x <= ℓmax, ell_eff))
+    for ib = 1:ibmax
+        # Calculate weights to clean the B-mode power spectrum
+        func_b(x) = -loglike_beta(nij, A(x), cb1[:, :, ib]) # -log(likelihood) to minimise by `optimize`
+        res = optimize(func_b, [-3, 1.6])
+        # println("B: ℓeff = ", ell_eff[ib])
+        # @show res
+        β = Optim.minimizer(res)
+        B = [synch.(ν, βs = β[1]) dust1.(ν, βd = β[2])] # Best-fitting frequency response of the FG
+        wb = milca_weights(cb2[:, :, ib], ones(nν), B)
+        # Calculate weights to clean the E-mode power spectrum
+        func_e(x) = -loglike_beta(nij, A(x), ce1[:, :, ib]) # -log(likelihood) to minimise by `optimize`
+        res = optimize(func_e, [-3, 1.6])
+        # println("E: ℓeff = ", ell_eff[ib])
+        # @show res
+        β = Optim.minimizer(res)
+        B = [synch.(ν, βs = β[1]) dust1.(ν, βd = β[2])] # Best-fitting frequency response of the FG
+        we = milca_weights(ce2[:, :, ib], ones(nν), B)
+        # Obtain power spectra of clean maps of the CMB
+        ee1[ib, irz], bb1[ib, irz] =
+            ilc_clean_cij(ce1[:, :, ib], we), ilc_clean_cij(cb1[:, :, ib], wb)
+        ee2[ib, irz], bb2[ib, irz] =
+            ilc_clean_cij(ce2[:, :, ib], we), ilc_clean_cij(cb2[:, :, ib], wb)
+        ee3[ib, irz], bb3[ib, irz] =
+            ilc_clean_cij(ce3[:, :, ib], we), ilc_clean_cij(cb3[:, :, ib], wb)
     end
-    # Derivative used by `optimize`
-    function ∇func(x)
-        add1(iip) =
-            loglike_beta_deriv(
-                nij * weight[ip[iip]]^2,
-                A(x),
-                dAdβs(x[1]),
-                d[:, ip[iip], 1],
-            ) + loglike_beta_deriv(
-                nij * weight[ip[iip]]^2,
-                A(x),
-                dAdβs(x[1]),
-                d[:, ip[iip], 2],
-            )
-        add2(iip) =
-            loglike_beta_deriv(
-                nij * weight[ip[iip]]^2,
-                A(x),
-                dAdβd(x[2]),
-                d[:, ip[iip], 1],
-            ) + loglike_beta_deriv(
-                nij * weight[ip[iip]]^2,
-                A(x),
-                dAdβd(x[2]),
-                d[:, ip[iip], 2],
-            )
-        if multithreads
-            ∇lnlike1, ∇lnlike2 = Atomic{Float64}(0), Atomic{Float64}(0)
-            @threads for iip = 1:np
-                atomic_add!(∇lnlike1, add1(iip))
-                atomic_add!(∇lnlike2, add2(iip))
-            end
-            return [-∇lnlike1[], -∇lnlike2[]]
-        else
-            ∇lnlike1, ∇lnlike2 = sum(add1.(1:np)), sum(add2.(1:np))
-            return [-∇lnlike1, -∇lnlike2]
-        end
-    end
-    res = optimize(func, [-3, 1.6])
-    # res = optimize(func, ∇func, [-3, 1.6], inplace = false)
-    @show res
-    β = Optim.minimizer(res)
-    ## Calculate weights to clean the E- and B-mode power spectra
-    B = [synch.(ν, βs = β[1]) dust1.(ν, βd = β[2])] # Best-fitting frequency response of the FG
-    we, wb = milca_weights(ce2, ones(nν), B), milca_weights(cb2, ones(nν), B)
-    ee1[:, irz], bb1[:, irz] = ilc_clean_cij(ce1, we), ilc_clean_cij(cb1, wb)
-    ee2[:, irz], bb2[:, irz] = ilc_clean_cij(ce2, we), ilc_clean_cij(cb2, wb)
-    ee3[:, irz], bb3[:, irz] = ilc_clean_cij(ce3, we), ilc_clean_cij(cb3, wb)
     # Show power spectra for visual inspection
     if showresults
         p = plot(
@@ -398,7 +252,7 @@ for irz = 1:nrz
             xlab = L"\ell",
             ylab = L"\ell(\ell+1)C_\ell/2\pi",
             ylims = [2e-5, 20],
-            xlims = [30, 300],
+            xlims = [ℓmin, ℓmax],
             legend = :topleft,
         )
         p = plot!(
@@ -407,23 +261,52 @@ for irz = 1:nrz
             (rsim / rclass) * clt_th_binned[4, :],
             label = "True BB",
         )
-        p = plot!(ell_eff, ee1[:, irz], m = :circle, lab = "Cleaned EE")
-        p = plot!(ell_eff, bb1[:, irz], m = :circle, lab = "Cleaned BB")
-        p = plot!(ell_eff, ee2[:, irz], m = :diamond, lab = "Noise EE")
-        p = plot!(ell_eff, bb2[:, irz], m = :diamond, lab = "Noise BB")
-        p = plot!(ell_eff, ee3[:, irz], m = :star5, lab = "FG EE")
-        p = plot!(ell_eff, bb3[:, irz], m = :star5, lab = "FG BB")
+        p = plot!(
+            ell_eff[1:ibmax],
+            ee1[1:ibmax, irz],
+            m = :circle,
+            lab = "Cleaned EE",
+        )
+        p = plot!(
+            ell_eff[1:ibmax],
+            bb1[1:ibmax, irz],
+            m = :circle,
+            lab = "Cleaned BB",
+        )
+        p = plot!(
+            ell_eff[1:ibmax],
+            ee2[1:ibmax, irz],
+            m = :diamond,
+            lab = "Noise EE",
+        )
+        p = plot!(
+            ell_eff[1:ibmax],
+            bb2[1:ibmax, irz],
+            m = :diamond,
+            lab = "Noise BB",
+        )
+        p = plot!(
+            ell_eff[1:ibmax],
+            ee3[1:ibmax, irz],
+            m = :star5,
+            lab = "FG EE",
+        )
+        p = plot!(
+            ell_eff[1:ibmax],
+            bb3[1:ibmax, irz],
+            m = :star5,
+            lab = "FG BB",
+        )
         display(p)
     end
 end
 
 # %% Calculate the mean power spectra and variance
-ℓmin, ℓmax = 30, 260 # ℓ range for fitting
-me1, mb1 = zeros(nbands), zeros(nbands) # Mean total power spectra
-ve1, vb1 = zeros(nbands), zeros(nbands) # Variance of total power spectra
-me2, mb2 = zeros(nbands), zeros(nbands) # Mean noise power spectra
-me3, mb3 = zeros(nbands), zeros(nbands) # Mean residual FG power spectra
-for ib = 1:nbands
+me1, mb1 = zeros(ibmax), zeros(ibmax) # Mean total power spectra
+ve1, vb1 = zeros(ibmax), zeros(ibmax) # Variance of total power spectra
+me2, mb2 = zeros(ibmax), zeros(ibmax) # Mean noise power spectra
+me3, mb3 = zeros(ibmax), zeros(ibmax) # Mean residual FG power spectra
+for ib = 1:ibmax
     me1[ib], mb1[ib] = mean(ee1[ib, :]), mean(bb1[ib, :])
     ve1[ib], vb1[ib] = var(ee1[ib, :]), var(bb1[ib, :])
     me2[ib], mb2[ib] = mean(ee2[ib, :]), mean(bb2[ib, :])
