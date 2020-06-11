3b4b66b1f29be27a871742a950260fb2c0611d53
Eiichiro Komatsu <eiichirokomatsu@me.com>
2020-06-11 15:38:14 +0200

    add additional data sets and codes in data/

---
+++ examples/data/gen_pysm_maps.py
@@ -0,0 +1,14 @@
+import pysm3
+import pysm3.units as u
+import healpy as hp
+import numpy as np
+sky = pysm3.Sky(nside=256, preset_strings=["a1", "d1", "f1", "s1"], output_unit="uK_CMB")
+nu=[27, 39, 93, 145, 220, 225, 280, 350, 410, 850]
+for i in range (0,10):
+    map = sky.get_emission(nu[i] * u.GHz)
+    map_equ = pysm3.apply_smoothing_and_coord_transform(map, rot=hp.Rotator(coord="GC"))
+    # RING format
+    hp.write_map("map_equ_{:03d}ghz_r8_uKcmb.fits".format(nu[i]), map_equ, coord="C")
+    # NESTED format
+    map_nested = hp.reorder(map_equ, r2n=True)
+    hp.write_map("map_equ_{:03d}ghz_r8_nested_uKcmb.fits".format(nu[i]), map_nested, coord="C", nest=True)
